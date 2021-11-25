e2b4859d23817ae2496f2d68a865acfa50e3f757
rifkytharabasuki <rifkytharabasuki@gmail.com>
2021-11-25 19:52:21 +0700

    your message

---
+++ database/migrations/2021_08_27_035028_create_book_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBookTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',255);
+            $table->string('description',255);
+            $table->string('author',255);
+            $table->string('publisher',255);
+            $table->string('date_of_issue',255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\AuthController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+//Routes Login
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+//Routes Group Middleware
+Route::group(['middleware' => ['auth:sanctum']], function (){
+
+    //Routes User and Logout
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/me', [AuthController::class, 'me']);
+
+    //Routes Books
+    Route::get('books','BookController@index');
+    Route::post('books','BookController@store');
+    Route::get('books/{id}','BookController@show');
+    Route::put('books/{id}','BookController@update');
+    Route::delete('books/{id}','BookController@destroy');
+
+    //Routes Author
+    Route::get('authors','AuthorController@index');
+    Route::post('authors','AuthorController@store');
+    Route::get('authors/{id}','AuthorController@show');
+    Route::put('authors/{id}','AuthorController@update');
+    Route::delete('authors/{id}','AuthorController@destroy');
+
+});
+
+
+
+
